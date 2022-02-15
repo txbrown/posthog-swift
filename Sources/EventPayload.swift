1d5f75e3097c8ef7dea85f5d81a367fdda595aed
leoMehlig <leo@leomehlig.com>
2022-02-15 22:03:19 +0100

    Update EventPayload.swift

--- Sources/EventPayload.swift
+++ Sources/EventPayload.swift
@@ -11,19 +11,19 @@ struct EventPayload: Codable, Hashable, Comparable {
     enum EventType: String, Codable {
         case capture, alias
     }
-
+    
     var timestamp: Date = Date()
-
+    
     var messageId: String = UUID().uuidString
-
+    
     let distinctId: String
-
+    
     let event: String
-
+    
     let type: EventType
-
+    
     let properties: [String: AnyCodable]
-
+    
     init(event: String,
          distinctId: String,
          type: EventType = .capture,
@@ -38,17 +38,22 @@ struct EventPayload: Codable, Hashable, Comparable {
         } else {
             switch type {
             case .capture:
-            self.distinctId = distinctId
-            self.properties = EventPayload.context
-                .merging(featureFlags.map({ ("$feature/\($0.key)", $0.value) }), uniquingKeysWith: { $1 })
-                .merging(properties, uniquingKeysWith: { $1 })
+                self.distinctId = distinctId
+                var mergeredProperties = EventPayload.context
+                    .merging(featureFlags.map({ ("$feature/\($0.key)", $0.value) }), uniquingKeysWith: { $1 })
+                    .merging(properties, uniquingKeysWith: { $1 })
+                if let version = mergeredProperties["$app_version"] {
+                    mergeredProperties["$set_once"] = AnyCodable((mergeredProperties["$set_once"]?.value as? [String: AnyCodable] ?? [:])
+                        .merging(["$inital_app_version": version], uniquingKeysWith: { (prev, _ ) in prev }))
+                }
+                self.properties = mergeredProperties
             case .alias:
                 self.distinctId = distinctId
                 self.properties = properties
             }
         }
     }
-
+    
     static var context: [String: AnyCodable] {
         var context: [String: AnyCodable] = [:]
         let info = (Bundle.main.infoDictionary ?? [:]).merging(Bundle.main.localizedInfoDictionary ?? [:], uniquingKeysWith: { $1 })
@@ -56,19 +61,19 @@ struct EventPayload: Codable, Hashable, Comparable {
         context["$app_version"] = (info["CFBundleShortVersionString"] as? String)?.codable
         context["$app_build"] = (info["CFBundleVersion"] as? String)?.codable
         context["$app_namespace"] = Bundle.main.bundleIdentifier?.codable
-
+        
         context["$device_manufacturer"] = "Apple"
         context["$device_type"] = Device.type.codable
         context["$device_model"] = Device.model.codable
         context["$os_name"] = Device.system.codable
         context["$os_version"] = Device.systemVersion.codable
-
+        
         context["$screen_width"] = Double(Device.screenSize.width).codable
         context["$screen_height"] = Double(Device.screenSize.height).codable
-
+        
         context["$lib"] = "posthog-swift"
-        context["$lib_version"] = "1.2.0"
-
+        context["$lib_version"] = "1.3.0"
+        
         if let lang = Locale.current.languageCode {
             if let country = Locale.current.regionCode {
                 context["$local"] = "\(lang)-\(country)".codable
@@ -78,12 +83,10 @@ struct EventPayload: Codable, Hashable, Comparable {
             context["$language"] = lang.codable
         }
         context["$timezone"] = TimeZone.current.identifier.codable
-
-        //TODO: Add $network_cellular and $network_wifi
-
+        
         return context
     }
-
+    
     static var sensitiveContext: [String: AnyCodable] {
         var context: [String: AnyCodable] = [:]
         let info = (Bundle.main.infoDictionary ?? [:]).merging(Bundle.main.localizedInfoDictionary ?? [:], uniquingKeysWith: { $1 })
@@ -91,24 +94,24 @@ struct EventPayload: Codable, Hashable, Comparable {
         context["$app_version"] = (info["CFBundleShortVersionString"] as? String)?.codable
         context["$app_build"] = (info["CFBundleVersion"] as? String)?.codable
         context["$app_namespace"] = Bundle.main.bundleIdentifier?.codable
-
+        
         context["$lib"] = "posthog-swift"
-        context["$lib_version"] = "1.2.0"
-
+        context["$lib_version"] = "1.3.0"
+        
         return context
     }
-
+    
     static func < (lhs: EventPayload, rhs: EventPayload) -> Bool {
         return lhs.timestamp < rhs.timestamp
     }
-
-
+    
+    
     func hash(into hasher: inout Hasher) {
         hasher.combine(messageId)
     }
-
+    
     static func == (lhs: EventPayload, rhs: EventPayload) -> Bool {
         return lhs.messageId == rhs.messageId
     }
-
+    
 }
